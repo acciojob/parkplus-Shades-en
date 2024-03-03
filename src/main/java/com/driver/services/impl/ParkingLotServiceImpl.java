30d693eb7de566d79af561ba98d672134020f28a
Owais Iqbal <owaisiqbal2013@gmail.com>
2024-03-04 03:03:07 +0530

    trying add spot fix

--- src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
+++ src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
@@ -53,6 +53,7 @@ public class ParkingLotServiceImpl implements ParkingLotService {
 
         parkingLot.getSpotList().add(spot);
 
+        parkingLotRepository1.save(parkingLot);
         return spotRepository1.save(spot);
     }
 
